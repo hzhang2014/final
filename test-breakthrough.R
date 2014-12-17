43bc3b0a28b96a9ca32dc6454699c14f6a426f69
Hui Zhang <ppaohh@umich.edu>
2014-12-16 22:20:00 -0500

    edit test

--- test-breakthrough.R
+++ test-breakthrough.R
@@ -6,6 +6,8 @@ data <- read.xlsx("Radium.xlsx",sheetIndex=1)
 expect_that(data, is_a("data.frame"))
 
 #Test capacity function
+ #fails without "round"
+ #passes with "round"
 expect_that(capacity("Radium.xlsx",9.6,"Ra"),equals("Ion-exchange capacity for Ra is 0.154 nCi/g"))
 expect_that(capacity("Radium.xlsx",9.6,"Ba"),equals("Ion-exchange capacity for Ba is -0.079 m moles/g"))    #Here a negative value means in this Radium test, Ba is not adsorbed, but released.
 
